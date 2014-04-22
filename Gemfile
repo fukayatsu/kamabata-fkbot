89e6808bde9e6d7bdf8e7535a1797b231b0c3b53
fukayatsu <fukayatsu@gmail.com>
2014-04-23 06:30:31 +0900

    Add lita-google-images

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source "https://rubygems.org"
 
 gem "lita"
 gem "lita-idobata"
-gem "pusher-client", github: 'pusher/pusher-ruby-client'
+gem "lita-google-images"
+
+gem "pusher-client", github: 'pusher/pusher-ruby-client' # waiting for v0.5.1+
--- lita_config.rb
+++ lita_config.rb
@@ -1,8 +1,7 @@
 Lita.configure do |config|
   config.robot.log_level = :info
-  # config.robot.adapter = :shell
-  config.robot.adapter = :idobata
-  config.robot.name        = ENV["BOT_NAME"]
+  config.robot.adapter     = ENV["ADAPTER"] ? ENV["ADAPTER"].to_sym : :shell
+  config.robot.name        = ENV["BOT_NAME"] || 'lita'
   config.adapter.api_token = ENV["API_TOKEN"]
 
   ## Example: Set options for the Redis connection.

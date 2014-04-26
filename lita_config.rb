c000f5fe5ff59894f25f1a9b34d3b388a9c34ccc
fukayatsu <fukayatsu@gmail.com>
2014-04-26 14:09:56 +0900

    Update lita-pebbles

--- lita_config.rb
+++ lita_config.rb
@@ -4,13 +4,11 @@ Lita.configure do |config|
   config.robot.name        = ENV["BOT_NAME"] || 'lita'
   config.adapter.api_token = ENV["API_TOKEN"]
 
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
   config.redis.url = ENV["REDISTOGO_URL"]
   config.http.port = ENV["PORT"]
 
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  config.handlers.schedules.room              = ENV["BOT_ROOM_ID"]
+  config.handlers.schedules.should_sleep_at   = '0 0 * * * Asia/Tokyo'
+  config.handlers.schedules.should_wake_up_at = '0 8 * * * Asia/Tokyo'
+
 end

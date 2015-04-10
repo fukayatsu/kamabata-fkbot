fa2ba9356bccc2b9020e003b8de624c514fd923f
fukayatsu <fukayatsu@gmail.com>
2015-04-10 15:59:40 +0900

    bundle update

--- lita_config.rb
+++ lita_config.rb
@@ -1,14 +1,17 @@
 Lita.configure do |config|
-  config.robot.log_level = :info
-  config.robot.adapter     = ENV["ADAPTER"] ? ENV["ADAPTER"].to_sym : :shell
+  config.robot.log_level   = :info
   config.robot.name        = ENV["BOT_NAME"] || 'lita'
-  config.adapter.api_token = ENV["API_TOKEN"]
+  if ENV["ADAPTER"] == 'idobata'
+    config.robot.adapter = :idobata
+    config.adapters.idobata.api_token = ENV["API_TOKEN"]
+  else
+    config.robot.adapter = :shell
+  end
 
-  config.redis.url = ENV["REDISTOGO_URL"]
-  config.http.port = ENV["PORT"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.http.port   = ENV["PORT"] || 3456
 
   config.handlers.schedules.room              = ENV["BOT_ROOM_ID"]
   config.handlers.schedules.should_sleep_at   = '0 0 * * * Asia/Tokyo'
   config.handlers.schedules.should_wake_up_at = '0 8 * * * Asia/Tokyo'
-
 end
